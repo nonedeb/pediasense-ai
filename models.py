36de8dd3dcc2fa424b285b476558aa4525ea0473
nonedeb <oidan.school@gmail.com>
2026-03-20 16:27:57 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,31 @@
-
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150))
-    email = db.Column(db.String(150), unique=True)
-    role = db.Column(db.String(50))
+
+    name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+
+    role = db.Column(db.String(20), nullable=False)  # student / faculty
+
+    section = db.Column(db.String(50))
+    program = db.Column(db.String(100))
+
+    specialization = db.Column(db.String(150))
+    post_nominals = db.Column(db.String(100))
+
+    contact_number = db.Column(db.String(30))
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class ImportLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_type = db.Column(db.String(20))
+    file_name = db.Column(db.String(255))
+    success_count = db.Column(db.Integer)
+    error_count = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
